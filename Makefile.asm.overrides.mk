3541785ddb2c174c0fa1d10baa2aefacdd6c97eb
Sergii Shapar <sshapar@google.com>
2023-01-25 17:22:07 +0000

    Override base image repo
    Will take effect after  https://github.com/istio/istio/pull/42464 is merged
    
    Change-Id: I40cf049bedfe112ce6d28b30bb2772493672c967
    Reviewed-on: https://gke-internal-review.googlesource.com/c/istio/istio/+/588614
    MCP-Verified: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>
    Reviewed-by: Stewart Butler <stewartbutler@google.com>
    MDP-Verified: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>
    CloudESF-Verified: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>
    Tested-by: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>
    ASM-Verified: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>
    Prow-Auto-Submit: Sergii Shapar <sshapar@google.com>

--- Makefile.asm.overrides.mk
+++ Makefile.asm.overrides.mk
@@ -9,5 +9,8 @@ export AUTH_HEADER ?= Authorization: Bearer $(shell gcloud auth print-access-tok
 ISTIO_ENVOY_BASE_URL ?= https://storage.googleapis.com/asm-testing/istio/dev
 GOBUILDFLAGS := -tags="netgo,osusergo"
 
+BASE_VERSION := master-2023-01-20T13-05-48
+ISTIO_BASE_REGISTRY := gcr.io/asm-staging-images
+
 # cloudesf specific overrides
 -include cloudesf/Makefile.cloudesf.version.mk
