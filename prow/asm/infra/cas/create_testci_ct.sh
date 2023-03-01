116cb1de9d1311b0db07eac306dcc1149eb6640c
Shankar Ganesan <shankgan@google.com>
2023-03-01 20:07:30 +0000

    ASM Ci e2e: Create static CAS certificate templates for use in e2e tests
    Change-Id: I513ceff049ccc70b4d67091373391636ebf6706d
    Reviewed-on: https://gke-internal-review.googlesource.com/c/istio/istio/+/633320
    Reviewed-by: Stewart Butler <stewartbutler@google.com>
    Tested-by: Stewart Butler <stewartbutler@google.com>
    CloudESF-Verified: Stewart Butler <stewartbutler@google.com>
    MCP-Verified: Stewart Butler <stewartbutler@google.com>
    MDP-Verified: Stewart Butler <stewartbutler@google.com>
    ASM-Verified: Stewart Butler <stewartbutler@google.com>

---
+++ prow/asm/infra/cas/create_testci_ct.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+CT_PREFIX="asm-testci-cert-template"
+PROJECT="istio-prow-build"
+#PROJECT="liwenhao-project"
+
+declare -a regions=("asia-east1" "asia-northeast1" "asia-southeast1"
+   "australia-southeast1"
+   "europe-north1" "europe-west1" "europe-west2" "europe-west3" "europe-west4"
+   "us-central1" "us-east1" "us-east4" "us-west1" "us-west2" "us-west3" "us-west4")
+
+#declare -a regions=("us-west1")
+
+for region in "${regions[@]}"
+do
+  echo "---------------"
+  echo "${region}"
+  ct_id="${CT_PREFIX}-${region}"
+  echo "${ct_id}"
+
+  gcloud privateca templates create "${ct_id}" \
+    --predefined-values-file cert_template.yaml \
+    --copy-sans --no-copy-subject \
+    --identity-cel-expression "subject_alt_names.all(san, san.type == URI && san.value.startsWith('spiffe://'))" \
+    --location="${region}" \
+    --project="${PROJECT}" \
+    --copy-known-extensions="base-key-usage,extended-key-usage,ca-options"
+
+  echo "created cert_template ${ct_id}"
+done
+
+echo "Finished cert_template creation..."
