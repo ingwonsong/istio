5276dbbb9eb61b13936eb56b8955b533a17e63f8
Sergii Shapar <sshapar@google.com>
2023-05-03 14:12:55 +0000

    Revert "Patch config to skip grpc_stats for Cloud ESF"
    This reverts commit 3bfb31e6d022a187feb065aefe1e940e87b107be.
    
    Reason for revert: Causing unit test failures like
    
    ```
    === RUN   TestInboundListenerConfig/multiple_services
    2023-05-02T15:45:47.048757Z    info    model    reloading network gateways
        listener_test.go:2637: found diff: virtualInbound-catchall-http/mTLS HTTP: http should be equal:   []string{
                  "istio.metadata_exchange",
            +     "envoy.filters.http.grpc_stats",
                  "envoy.filters.http.fault",
                  ... // 2 identical elements
              }
            
            Left: [istio.metadata_exchange envoy.filters.http.grpc_stats envoy.filters.http.fault envoy.filters.http.cors envoy.filters.http.router]
            Right: [istio.metadata_exchange envoy.filters.http.grpc_stats envoy.filters.http.grpc_stats envoy.filters.http.fault envoy.filters.http.cors envoy.filters.http.router]
    ```
    
    Change-Id: Ide481c5a6c18f167706db3bc5342502448411ab6
    Reviewed-on: https://gke-internal-review.googlesource.com/c/istio/istio/+/699258
    ASM-Verified: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>
    Tested-by: Sergii Shapar <sshapar@google.com>
    MDP-Verified: Sergii Shapar <sshapar@google.com>
    CloudESF-Verified: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>
    Reviewed-by: Teju Nareddy <nareddyt@google.com>
    MCP-Verified: Sergii Shapar <sshapar@google.com>

--- pilot/pkg/networking/core/v1alpha3/listener_builder.go
+++ pilot/pkg/networking/core/v1alpha3/listener_builder.go
@@ -393,10 +393,6 @@ func (lb *ListenerBuilder) buildHTTPConnectionManager(httpOpts *httpListenerOpts
 		filters = append(filters, xdsfilters.GrpcStats)
 	}
 
-	if !features.EnableCloudESF {
-		filters = append(filters, xdsfilters.GrpcStats)
-	}
-
 	// append ALPN HTTP filter in HTTP connection manager for outbound listener only.
 	if features.ALPNFilter {
 		if httpOpts.class != istionetworking.ListenerClassSidecarInbound {
