9efad0866d259933eaf9393a0fb789187d98eb07
Rui Gu <ruigu@google.com>
2023-12-12 05:41:38 +0000

    Fix fallback logic for using CGW for public clusters
    For legacy control planes that has no memberships, always use direct IP access.
    When ENABLED_WITHOUT_FALLBACK is set, we disable the fallback for all
    the clusters with hub membership. This way, we return error directly
    when membership is deleted instead of proceeding and hit the VPCSC
    timeout wall.
    
    Change-Id: I6218ab1afd779228d12bcdf4d74704d8d575c658
    Reviewed-on: https://gke-internal-review.googlesource.com/c/istio/istio/+/888047
    MDP-Verified: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>
    Reviewed-by: Piotr Kufel <qfel@google.com>
    Tested-by: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>

--- pilot/cmd/pilot-discovery/app/mcpinit/mcpinit.go
+++ pilot/cmd/pilot-discovery/app/mcpinit/mcpinit.go
@@ -134,7 +134,7 @@ func ConstructKubeConfigFile(ctx context.Context, p KubeConfigParameters) (*cont
 			endpoint = strings.TrimPrefix(cgwURL, "https://")
 			caCertificate = ""
 		}
-	} else if asm.ConnectGatewayForPublicCluster() != asm.CGWForPublicClusterDisabled {
+	} else if p.HubMembership != "" && asm.ConnectGatewayForPublicCluster() != asm.CGWForPublicClusterDisabled {
 		cgwURL, err := connectGatewayURL(ctx, p.FleetProjectNumber, p.HubMembership)
 		err = fmt.Errorf("failed to setup Connect Gateway for public cluster: %w", err)
 		if err != nil {
