c81ec08022f1d53bb8e88a8e5855507a9d0c3928
Ingwon Song <igsong@google.com>
2024-03-21 01:03:21 +0000

    Allow unknown types in config dump
    Change-Id: I2e41807f494e2fa1a7765bb630eef0124a1c8995

--- istioctl/pkg/writer/envoy/configdump/configdump.go
+++ istioctl/pkg/writer/envoy/configdump/configdump.go
@@ -43,13 +43,10 @@ func SetPrintConfigTypeInSummary(p bool) {
 
 // Prime loads the config dump into the writer ready for printing
 func (c *ConfigWriter) Prime(b []byte) error {
-	cd := &adminv3.ConfigDump{}
-	err := protomarshal.UnmarshalWithGlobalTypesResolver(b, cd)
+	c.configDump = &configdump.Wrapper{}
+	err := c.configDump.UnmarshalJSON(b)
 	if err != nil {
 		return fmt.Errorf("error unmarshalling config dump response from Envoy: %v", err)
-	}
-	c.configDump = &configdump.Wrapper{
-		ConfigDump: cd,
 	}
 	return nil
 }
